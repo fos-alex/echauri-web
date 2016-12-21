4e7e39c0d947c2db2324c883675de2a370a7e2b4
Alex Otero <alex.otero@globant.com>
2016-12-21 12:01:03 -0300

    Configure email sending

--- src/back/app.js
+++ src/back/app.js
@@ -1,36 +1,44 @@
 var nodemailer = require('nodemailer');
+var express = require('express');
+var ses = require('nodemailer-ses-transport');
 
+var app = express();
 var router = express.Router();
-app.use('/sayHello', router);
-router.post('/', handleSayHello); // handle the route at yourdomain.com/sayHello
+app.use('/send-email', router);
+router.get('/', handleSendEmail); // handle the route at yourdomain.com/sayHello
 
-function handleSayHello(req, res) {
-    // Not the movie transporter!
-    var transporter = nodemailer.createTransport({
-        service: 'Gmail',
-        auth: {
-            user: 'web@echaurivinos.com.ar', // Your email id
-            pass: 'password' // Your password
-        }
-    });
+app.listen(4000, function () {
+    console.log('Example app listening on port 4000!')
+});
 
-    var text = 'Hello world from \n\n' + req.body.name;
+console.log('Connecting to ses');
+var transporter = nodemailer.createTransport(ses({
+    accessKeyId: 'AKIAJSPBGMEGVWBE6PKQ',
+    secretAccessKey: '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',
+    "region": "us-east-1"
+}));
+
+function handleSendEmail(req, res) {
+
+
+    var text = 'Hello world from';
 
     var mailOptions = {
-        from: 'example@gmail.com>', // sender address
-        to: 'receiver@destination.com', // list of receivers
+        from: 'info@echaurivinos.com.ar', // sender address
+        to: 'fos.alex@gmail.com', // list of receivers
         subject: 'Email Example', // Subject line
         text: text //, // plaintext body
         // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
     };
 
+    console.log('Sending email');
     transporter.sendMail(mailOptions, function(error, info){
-        if(error){
+        if (error) {
             console.log(error);
             res.json({yo: 'error'});
-        }else{
+        } else {
             console.log('Message sent: ' + info.response);
             res.json({yo: info.response});
-        };
+        }
     });
 }
